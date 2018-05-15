ae42b4ec66c4d79a48f011422d8cb7feb73d14d9
anubhav100 <anubhav.tarar@knoldus.in>
2018-05-15 15:14:01 +0530

    added controller for user registration

--- user-api/src/test/scala/com/knoldus/controller/UserControllerTest.scala
+++ user-api/src/test/scala/com/knoldus/controller/UserControllerTest.scala
@@ -1,50 +1,55 @@
 package com.knoldus.controller
 
-import akka.http.javadsl.model.ContentTypes
 import akka.http.scaladsl.model.{HttpEntity, MediaTypes}
-import akka.http.scaladsl.server.ContentNegotiator.Alternative.ContentType
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.util.ByteString
+import com.knoldus.domains.User
 import com.knoldus.services.UserService
 import com.knoldus.util.JsonHelper
+import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{Matchers, WordSpec}
 
-class UserControllerTest extends WordSpec with JsonHelper with MockitoSugar with Matchers with ScalatestRouteTest {
+
+class UserControllerTest
+  extends WordSpec with JsonHelper with MockitoSugar with Matchers with ScalatestRouteTest {
+
+
+  val routes = UserTest.userRoutes
+  val user = new User("anubhav.tarar@knoldus.in", None)
+  val userResponse = new User("anubhav.tarar@knoldus.in", user.userType)
+  val jsonRequest =
+    """{
+        "emailId":"anubhav.tarar@knoldus.in"
+        }""".stripMargin
+  when(UserTest.userService.createUser(user)) thenReturn (userResponse)
+  val invalidJsonRequest =
+    """{
+        "emailId":""
+        }""".stripMargin
+  val entity = HttpEntity(MediaTypes.`application/json`, jsonRequest)
 
   object UserTest extends UserController {
     val userService = mock[UserService]
   }
 
-  val routes = UserTest.userRoutes
-
-  val jsonRequest = ByteString(
-    s"""
-       |{
-       |    "emailId": "anubhav.tarar@knoldus.in"
-       |}
-        """.stripMargin
-  )
-
-  val entity = HttpEntity(MediaTypes.`application/json`, jsonRequest)
-
-
   "The User routes" should {
 
-    " user creation should success with correct data" in
-    pending
-      Post("/kip/createusers",entity) ~> routes ~> check {
-  // println(responseAs[String] )
+    " user creation should success with correct data" in {
+      Post("/kip/createusers", entity) ~> routes ~> check {
+        handled shouldBe true
+        val result = responseAs[String]
+        result shouldEqual """{"data":{"emailId":"anubhav.tarar@knoldus.in"}}"""
 
-// println("***************"+responseAs[String])
-/* responseAs[String] shouldEqual
-{
-|    "data": {
-|        "emailId": "anubhav.tarar@knoldus.in",
-|        "userType": "trainee"
-|    }
-|}*/
-}
+      }
+    }
+    " user creation should not success with incorrect data" in {
+      val newEntity = HttpEntity(MediaTypes.`application/json`, invalidJsonRequest)
+      Post("/kip/createusers", newEntity) ~> routes ~> check {
+        handled shouldBe true
+        val result = responseAs[String]
+        result shouldEqual """{"message":"Invalid Json Field"}"""
+      }
+    }
+  }
 }
 
-}
