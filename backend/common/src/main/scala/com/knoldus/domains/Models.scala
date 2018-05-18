db63d7fcd185823c734e98f3334e6ea6f80a85a7
Aakash06 <aakash.jain@knoldus.in>
2018-05-18 10:11:15 +0530

    rebas with master

--- backend/common/src/main/scala/com/knoldus/domains/Models.scala
+++ backend/common/src/main/scala/com/knoldus/domains/Models.scala
@@ -61,6 +61,27 @@ object DateMarshalling {
     Try{ localIsoDateFormatter.get().parse(date) }.toOption
 }
 
+object DateMarshalling {
+  implicit object DateFormat extends JsonFormat[Date] {
+    def write(date: Date) = JsString(dateToIsoString(date))
+    def read(json: JsValue) = json match {
+      case JsString(rawDate) =>
+        parseIsoDateString(rawDate)
+          .fold(throw new Exception(s"Expected ISO Date format, got $rawDate"))(identity)
+    }
+  }
+
+  private val localIsoDateFormatter = new ThreadLocal[SimpleDateFormat] {
+    override def initialValue() = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+  }
+
+  private def dateToIsoString(date: Date) =
+    localIsoDateFormatter.get().format(date)
+
+  private def parseIsoDateString(date: String): Option[Date] =
+    Try{ localIsoDateFormatter.get().parse(date) }.toOption
+}
+
 case object UserInfo extends DefaultJsonProtocol with SprayJsonSupport {
   implicit val userProtocol: RootJsonFormat[UserInfo] = jsonFormat2(UserInfo.apply)
 }
