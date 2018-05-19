8edc4ef00a906895c9a771cf767378ae4618b89f
Aakash06 <aakash.jain@knoldus.in>
2018-05-19 05:42:36 +0530

    Add route for kip sessions

--- frontend/src/app/session/session-routing.module.ts
+++ frontend/src/app/session/session-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {CreateSessionComponent} from './create-session/create-session.component';
-import {UpdateSessionComponent} from './update-session/update-session.component';
 import {LogggedInGuard} from '../user/loggged-in.guard';
 import {ListSessionComponent} from './list-session/list-session.component';
 import {SessionComponent} from "./session.component";
